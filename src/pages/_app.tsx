b3a4c14e35981bcd4379821148c70dcfd7c4a92e
Paurush Batish <paurushbatishfbd@gmail.com>
2023-07-18 11:56:36 -0400

    cookie set by adding headers

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,16 +2,23 @@ import { ChakraProvider } from '@chakra-ui/react'
 
 import theme from '../theme'
 import { AppProps } from 'next/app'
-import { Client, Provider ,cacheExchange, fetchExchange} from 'urql'
+import { Client, Provider , fetchExchange, cacheExchange} from 'urql'
+
 
 const client = new Client({
   url: 'http://localhost:4000/graphql',
-  exchanges: [cacheExchange, fetchExchange],
-  fetchOptions:{
-    credentials:"include",
-    
+  exchanges: [
+    fetchExchange,
+    cacheExchange
 
-  }
+  ],
+  
+  fetchOptions:{
+    credentials:  "include",
+    headers: {
+      'x-forwarded-proto': 'https', /// to set cookie in browser
+    },
+  },
 });
 
 function MyApp({ Component, pageProps }: AppProps) {
@@ -26,3 +33,4 @@ function MyApp({ Component, pageProps }: AppProps) {
 }
 
 export default MyApp
+
